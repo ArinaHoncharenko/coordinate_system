bfdf8d95aef44a81cd49109f958eefa38803a325
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-09-28 19:22:13 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace oop4_coordinate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(121, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox2.Location = new System.Drawing.Point(121, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.calculateButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
+            this.calculateButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.calculateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.calculateButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.calculateButton.Location = new System.Drawing.Point(121, 143);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(117, 41);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calulate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(319, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 192);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Location = new System.Drawing.Point(35, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter x";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label2.Location = new System.Drawing.Point(35, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter y";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.resultLabel.Location = new System.Drawing.Point(35, 240);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 25);
+            this.resultLabel.TabIndex = 6;
+            this.resultLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(571, 295);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private System.Windows.Forms.Label label2;
+
+        private void label3_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace oop4_coordinate
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Отримуємо значення координат x та y з текстових полів
+                double x = double.Parse(textBox1.Text);
+                double y = double.Parse(textBox2.Text);
+
+                string quadrant;
+                Color pointColor;
+
+                // Визначаємо четверть системи координат
+                if (x > 0 && y > 0)
+                {
+                    quadrant = "в I";
+                    pointColor = Color.Red;
+                }
+                else if (x < 0 && y > 0)
+                {
+                    quadrant = "в II";
+                    pointColor = Color.Blue;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    quadrant = "в III";
+                    pointColor = Color.Green;
+                }
+                else if (x > 0 && y < 0)
+                {
+                    quadrant = "в IV";
+                    pointColor = Color.Orange;
+                }
+                else if (x == 0 && y != 0)
+                {
+                    quadrant = "на вісі Y";
+                    pointColor = Color.Purple;
+                }
+                else if (x != 0 && y == 0)
+                {
+                    quadrant = "на вісі X";
+                    pointColor = Color.Magenta;
+                }
+                else
+                {
+                    quadrant = "в початку координат";
+                    pointColor = Color.Black;
+                }
+
+                // Відображення результату
+                resultLabel.Text = $"Точка ({x}, {y}) знаходиться {quadrant} чверті.";
+
+                // Візуальне відображення системи координат та точки з визначеним кольором
+                DrawCoordinateSystem(x, y, pointColor);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некоректні вхідні дані. Будь ласка, введіть числа для координат x та y.");
+            }
+        }
+
+        private void DrawCoordinateSystem(double x, double y, Color pointColor)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+
+            // Малювання осей координат
+            g.DrawLine(Pens.Black, centerX, 0, centerX, pictureBox1.Height); // Вертикальна вісь
+            g.DrawLine(Pens.Black, 0, centerY, pictureBox1.Width, centerY); // Горизонтальна вісь
+            // Масштабування координат, щоб відобразити точку в системі координат
+            int pointX = centerX + (int)(x * 10); // Масштаб 1:10
+            int pointY = centerY - (int)(y *10); // Масштаб 1:10
+
+            // Малювання точки з визначеним кольором
+            using (Brush brush = new SolidBrush(pointColor))
+            {
+                g.FillEllipse(brush, pointX - 2, pointY - 2, 5, 5);
+            }
+        }
+    }
+}
